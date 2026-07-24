c7c45188e91ebfe528858ec00160c50e8853ed27
HexaVox7 <luigi050701@gmail.com>
2026-07-24 15:32:12 -0700

    Add root endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import FruitList from './components/Fruits';
 
 const App = () => {
   return (
-    <div>
-
+    <div className="App">
+      <header className="App-header">
+        <h1>Fruit Management App</h1>
+      </header>
+      <main>
+        <FruitList />
+      </main>
     </div>
   );
 };
